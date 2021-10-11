f3cc8e2cab39b5267147fee2676af9e5b6e08525
Róbert Szinay <rszinay@email.cz>
2021-10-11 13:49:53 +0200

    Adds author resource

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/books', 'App\Http\Controllers\BooksController@store');
 Route::patch('/books/{book}', 'App\Http\Controllers\BooksController@update');
+Route::delete('/books/{book}', 'App\Http\Controllers\BooksController@destroy');
+
+Route::post('/author', 'App\Http\Controllers\AuthorsController@store');
