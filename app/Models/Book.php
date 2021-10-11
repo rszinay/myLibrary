73089d52db516f3445632a1502fbca075597f8a0
Róbert Szinay <rszinay@email.cz>
2021-10-11 13:49:12 +0200

    Adds new path to book resource

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     protected $guarded = [];
+
+    public function path()
+    {
+        return '/books/' . $this->id;
+    }
 }
