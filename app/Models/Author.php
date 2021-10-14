ddd87ecae9f379c402c38a331840a0dc6d6e6042
Róbert Szinay <rszinay@email.cz>
2021-10-14 11:30:16 +0200

    Adds book reservation functionality

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     protected $dates = ['dob'];
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,13 +5,53 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use App\Models\Author;
+use App\Models\Reservation;
 
 class Book extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     public function path()
     {
         return '/books/' . $this->id;
+    }
+
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id'   =>  $user->id,
+            'checked_out_at'    =>  now()
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if(is_null($reservation)) {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);
+    }
+
+    public function setAuthorIdAttribute($author)
+    {
+        $this->attributes['author_id'] = (Author::firstOrCreate([
+            'name'  => $author
+        ]))->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
